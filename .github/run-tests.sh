62968beb646354801ceb5ac8a1d8e74a2b7827a0
Ashay <clearkruti@gmail.com>
2020-05-10 17:48:32 +0530

    Added github actions to feat_2 on PR

---
+++ .github/run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+run_test () {
+	echo "$1 OK."
+}
+
+# Deliberate error exit
+exit 1
+
+
+ALL_TESTS=("../tests/unit_test1.sh" "../tests/unit_test1.sh")
+
+for I in ${ALL_TESTS[@]}; do
+	run_test $I
+done
